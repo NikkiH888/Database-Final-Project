d36373b1cc6577247cf12fe7a0a60e4dd13970d9
John Umstead <31291580+JohnUmstead@users.noreply.github.com>
2018-04-29 15:43:23 -0500

    Update New_Queries.sql

--- New_Queries.sql
+++ New_Queries.sql
@@ -11,10 +11,12 @@ SELECT s.contractID, t.title, s.occurences
 FROM ContractServices s
 JOIN ServiceType t ON s.contractservice_ID = t.contractservice_ID;
 
+# Gross Profit Margin - showing totals for estimated costs and actual costs
 SELECT TotalCostEstimate, ActualCostEstimate
 FROM Contracts
 GROUP BY ContractYear;
 
+# Gross Profit Margin - Showing profit margins from contracts for each year.
 SELECT ContractYear, (Select ActualCostEstimate From Contracts Group by ContractYear)/(Select SalePrice From Contracts Group by ContractYear) as Contribution_Margin
 FROM Contracts
 ORDER BY ContractYear Desc;
