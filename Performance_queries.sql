e8fe0720a0d9c1aec0d24f0f81725005c16af0eb
Benjavicha Hotrabhavananda <benjavicha1@gmail.com>
2018-05-02 06:32:31 -0500

    update

--- Performance_queries.sql
+++ Performance_queries.sql
@@ -4,13 +4,16 @@
            (SUM(sales_price) - SUM(actualCost)) / SUM(sales_price) AS "Profit Margin"                     
  FROM Contract;
 
-
  # Company Performance - Retrieve the total revenue & expense of the company each year
  SELECT contractYear, SUM(sales_price), SUM(actualCost)
  FROM Contract 
  GROUP BY contractYear
  Order BY ContractYear ASC;
- 
+                                   
+# Company Performance - get the min and max year                           
+SELECT extract(year from MAX(day))::integer
+from sailors S, reserves R, boats B
+where S.sid = R.sid AND R.bid=B.bid;
  
  SELECT Branch_ID, SUM(Contract_price)
 FROM Property P
