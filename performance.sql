de81680dd3f1be839f37256c722fa7ba78d3f2d4
Benjavicha Hotrabhavananda <benjavicha1@gmail.com>
2018-05-02 23:11:30 -0500

    Update

--- performance.sql
+++ performance.sql
@@ -14,18 +14,15 @@
 SELECT MIN(contractYear), MAX(contractYear)
 FROM contract;
  
- SELECT Branch_ID, SUM(Contract_price)
-FROM Property P
-INNER JOIN Contract C
-ON P.property_ID = C.property_ID
-INNER JOIN Branch B
-ON B.branch_ID = P.branch_ID
-GROUP BY Branch_ID;
+SELECT B.Branch_ID, C.contractYear, SUM(sale_price),  SUM(actualCost)
+FROM Contract C, Property P,  Branch B
+WHERE C.property_ID = P.property_ID AND P.branch_ID = B.branch_ID
+GROUP BY B.Branch_ID, C.contractYear;
  
-# Gross Profit Margin - showing totals for estimated costs and actual costs
-SELECT TotalCostEstimate, ActualCostEstimate
-FROM Contracts
-GROUP BY ContractYear;
+-- # Gross Profit Margin - showing totals for estimated costs and actual costs
+-- SELECT TotalCostEstimate, ActualCostEstimate
+-- FROM Contracts
+-- GROUP BY ContractYear;
 
 # Contribution Margin - Showing profit margins from contracts for each year.
 SELECT ContractYear, 1 - ((Select sum(ActualCostEstimate) From Contracts Group by ContractYear)/(Select sum(sale_price) From Contracts Group by ContractYear)) as Contribution_Margin
