749b1e9cfcf14603cd9131772da01f897b8bd155
Benjavicha Hotrabhavananda <benjavicha1@gmail.com>
2018-05-02 21:27:30 -0500

    update

--- performance.sql
+++ performance.sql
@@ -5,15 +5,14 @@
  FROM Contract;
 
  # Company Performance - Retrieve the total revenue & expense of the company each year
- SELECT contractYear, SUM(sales_price), SUM(actualCost)
+ SELECT contractYear, SUM(sale_price), SUM(actualCost)
  FROM Contract 
  GROUP BY contractYear
  Order BY ContractYear ASC;
                                    
 # Company Performance - get the min and max year                           
-SELECT extract(year from MAX(day))::integer
-from sailors S, reserves R, boats B
-where S.sid = R.sid AND R.bid=B.bid;
+SELECT MIN(contractYear), MAX(contractYear)
+FROM contract;
  
  SELECT Branch_ID, SUM(Contract_price)
 FROM Property P
